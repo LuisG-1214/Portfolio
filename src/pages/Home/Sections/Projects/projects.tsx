20bb66891507376671b6077f83bd08685bc08316
LuisG-1214 <luisgustavo.webdev@gmail.com>
2025-01-02 19:35:27 -0300

    Feat:projects area are done

--- src/pages/Home/Sections/Projects/projects.tsx
+++ src/pages/Home/Sections/Projects/projects.tsx
@@ -1,6 +1,8 @@
 import { Container, styled, Typography, Box } from "@mui/material";
 import  Grid  from '@mui/material/Grid2';
 import { motion } from "framer-motion";
+import Project1 from "../../../../assets/images/palette1.jpg";
+import Project2 from "../../../../assets/images/capa.jpg";
 
 
 const Projects = () => {
@@ -8,6 +10,14 @@ const Projects = () => {
   const StyledProjects = styled("div") (({theme}) => ({
     backgroundColor: theme.palette.primary.main,
     display: "flex",
+  }))
+
+  const StyledImg = styled("img") (({theme}) => ({
+    width: "90%",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
+    [theme.breakpoints.down('md')]: {
+        width: "50%", 
+    },
   }))
 
   const boxVariants = (direction: "left" | "right") => ({
@@ -39,10 +49,10 @@ const Projects = () => {
               gutterBottom 
               fontFamily="montserrat"
             >
-              Projects
+              Projetos
             </Typography>
 
-            <Grid container spacing={4}> 
+            <Grid container spacing={4} py={6}> 
               <Grid size={{xs:12 , md:6}}>
               <motion.div
                 variants={boxVariants("left")}
@@ -58,9 +68,34 @@ const Projects = () => {
                     borderRadius: 1,
                     textAlign: "center",
                     height: {xs: "30rem", md: "40rem"},
+                    transition: "box-shadow 0.3s ease-in-out",
                   }}
                 >
+                  <Typography 
+                    sx={{
+                      py: 3,
+                      textAlign: "center",
+                      color: "#ffffff",
+                    }}
+                    variant="h4"  
+                    textAlign="center" 
+                    fontFamily="montserrat">
+                      Palette Creator
+                    </Typography>
 
+                    <StyledImg src={Project1} />
+
+                    <Typography 
+                      sx={{
+                        py: 6,
+                        px: 3.5,
+                        textAlign: "justify",
+                        color: "#ffffff",
+                      }}
+                      variant="body1" 
+                      fontFamily="montserrat">
+                        Desenvolvi uma aplicação web que cria paletas de cores personalizadas com 16 tonalidades únicas. Os usuários selecionam três cores primárias, e a aplicação gera uma paleta diversificada variando aleatoriamente o tom e a saturação, mantendo a luminosidade original. Este projeto aproveita as capacidades do HTML5, CSS3 e JavaScript para oferecer uma experiência de usuário visualmente atraente e interativa.
+                      </Typography>  
                 </Box>
               </motion.div>
                 
@@ -81,15 +116,40 @@ const Projects = () => {
                     borderRadius: 1,
                     textAlign: "center",
                     height: {xs: "30rem", md: "40rem"},
+                    transition: "0.3s",
                   }} 
                 >
+                  <Typography 
+                    sx={{
+                      py: 3,
+                      textAlign: "center",
+                      color: "#ffffff",
+                    }}
+                    variant="h4"  
+                    textAlign="center" 
+                    fontFamily="montserrat">
+                      Dev Burguer
+                    </Typography>
 
+                    <StyledImg src={Project2} />
+
+                    <Typography 
+                      sx={{
+                        py: 6,
+                        px: 3.5,
+                        textAlign: "justify",
+                        color: "#ffffff",
+                      }}
+                      variant="body1" 
+                      fontFamily="montserrat">
+                        Desenvolvido durante um curso de Tailwind CSS e JavaScript, este projeto é um sistema simulado de cardápio e carrinho de compras de uma lanchonete. Inclui funcionalidades para adicionar e remover itens do carrinho e redirecionar os usuários para o WhatsApp para finalizar seus pedidos. Tecnologias utilizadas: HTML5, Tailwind CSS e JavaScript.
+                      </Typography>
                 </Box>              
               </motion.div>
                 
               </Grid>
 
-              <Grid size={{xs:12 , md:6}}>
+              {/* <Grid size={{xs:12 , md:6}}>
               <motion.div
                 variants={boxVariants("left")}
                 initial="hidden"
@@ -106,7 +166,31 @@ const Projects = () => {
                     height: {xs: "30rem", md: "40rem"},
                   }}
                 >
+                  <Typography 
+                    sx={{
+                      py: 3,
+                      textAlign: "center",
+                      color: "#ffffff",
+                    }}
+                    variant="h4"  
+                    textAlign="center" 
+                    fontFamily="montserrat">
+                      Palette Creator
+                    </Typography>
 
+                    <StyledImg src={Project1} />
+
+                    <Typography 
+                      sx={{
+                        py: 6,
+                        px: 3.5,
+                        textAlign: "justify",
+                        color: "#ffffff",
+                      }}
+                      variant="body1" 
+                      fontFamily="montserrat">
+                        Developed a web application that creates custom color palettes of 16 unique hues. Users select three primary colors, and the application generates a diverse palette by randomly varying the hue and saturation while preserving the original luminosity. This project leverages the capabilities of HTML5, CSS3, and JavaScript to deliver a visually appealing and interactive user experience.
+                      </Typography>
                 </Box>
               </motion.div>
                 
@@ -129,12 +213,36 @@ const Projects = () => {
                     height: {xs: "30rem", md: "40rem"},
                   }} 
                 >
+                  <Typography 
+                    sx={{
+                      py: 3,
+                      textAlign: "center",
+                      color: "#ffffff",
+                    }}
+                    variant="h4"  
+                    textAlign="center" 
+                    fontFamily="montserrat">
+                      Palette Creator
+                    </Typography>
 
+                    <StyledImg src={Project1} />
+
+                    <Typography 
+                      sx={{
+                        py: 6,
+                        px: 3.5,
+                        textAlign: "justify",
+                        color: "#ffffff",
+                      }}
+                      variant="body1" 
+                      fontFamily="montserrat">
+                        Developed a web application that creates custom color palettes of 16 unique hues. Users select three primary colors, and the application generates a diverse palette by randomly varying the hue and saturation while preserving the original luminosity. This project leverages the capabilities of HTML5, CSS3, and JavaScript to deliver a visually appealing and interactive user experience.
+                      </Typography>
                 </Box>              
               </motion.div> 
-                
-              </Grid>
-            </Grid>
+                 */}
+              {/* </Grid>*/}
+            </Grid> 
 
           </Container>
         </StyledProjects>
