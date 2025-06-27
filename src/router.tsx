47e0f6e3c2811ac7f11185cdc43ba0db3dc65d5b
LuisG-1214 <luisgustavo.webdev@gmail.com>
2025-06-26 23:55:07 -0300

    teste de rota

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home/home";
 import Certificates from "../src/pages/Certificates/certificates";
 
